e2fa3748d963635b7b0794dece629852744ee949
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-01-08 23:16:37 +0530

    button component : cursor-pointer

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import "./Button.css"
 
 function Button({isUpdateButton, placeholder, onClick}) {
   return (
-    <div className={`${isUpdateButton ? 'whiteButton ' : 'blackButton '} flex flex-col justify-center items-center`} onClick={onClick}>
+    <div className={`${isUpdateButton ? 'whiteButton ' : 'blackButton '} flex flex-col justify-center items-center cursor-pointer`} onClick={onClick}>
       <p className={`${isUpdateButton ? 'text-black ' : 'text-white '} buttonText`}>{placeholder}</p>
     </div>
   )

fc058e23f4368e47c642447a44f7eaaed32f6df2
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-01-08 23:15:48 +0530

    tailwind initialized + button component

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Button.css"
+
+function Button({isUpdateButton, placeholder, onClick}) {
+  return (
+    <div className={`${isUpdateButton ? 'whiteButton ' : 'blackButton '} flex flex-col justify-center items-center`} onClick={onClick}>
+      <p className={`${isUpdateButton ? 'text-black ' : 'text-white '} buttonText`}>{placeholder}</p>
+    </div>
+  )
+}
+
+export default Button

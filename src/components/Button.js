ddfbea48c2da7a8d725bbd471e43dfb2572e6bf7
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-01-08 23:18:56 +0530

    button component : select-none

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import "./styles/Button.css"
 function Button({isUpdateButton, placeholder, onClick}) {
   return (
     <div className={`${isUpdateButton ? 'whiteButton ' : 'blackButton '} flex flex-col justify-center items-center cursor-pointer`} onClick={onClick}>
-      <p className={`${isUpdateButton ? 'text-black ' : 'text-white '} buttonText`}>{placeholder}</p>
+      <p className={`${isUpdateButton ? 'text-black ' : 'text-white '} buttonText select-none`}>{placeholder}</p>
     </div>
   )
 }

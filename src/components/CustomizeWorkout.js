8119a0612cbe851de8f9ea6cd888acb82246f7cf
SaurabhSahu01 <saurabhsahu794@gmail.com>
2024-01-10 22:34:14 +0530

    component : CustomizWorkout - overflow-y-scroll

--- src/components/CustomizeWorkout.js
+++ src/components/CustomizeWorkout.js
@@ -17,11 +17,11 @@ function CustomiseWorkout({setOpenModal}) {
   }, [value])
 
   return (
-    <div className='bg-[#141414] h-[100vh] w-full z-[1000] fixed top-0 flex flex-col justify-start items-center'>
+    <div className='bg-[#141414] h-[100vh] w-full z-[1000] fixed top-0 flex flex-col justify-start items-center overflow-y-scroll'>
       <img src={crossicon} alt="crossicon" className='w-[26px] h-[26px] absolute top-5 right-4' onClick={() => setOpenModal(false)}/>
       <h2 className='heading mt-[35px]'>Customize your workout</h2>
       <div className='h-full flex flex-col justify-around items-center mb-5'>
-        <div className='h-full flex flex-col justify-start items-center gap-5 mt-[100px]'>
+        <div className='h-full flex flex-col justify-start items-center gap-5 mt-[100px] overflow-y-scroll'>
           <CustomSelect selectName={"THEME"} options={options} setValue={setValue} isDropdown={false}/>
           <CustomSelect selectName={"Equipment(Optional)"} options={options} setValue={setValue} />
           <CustomSelect selectName={"Workout Duration"} options={options} setValue={setValue} />
